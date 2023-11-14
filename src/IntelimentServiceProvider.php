dfe2422c3cb2cee1286d8670e46d12d0c9f22bfe
chrisreedio <chrisreedio@users.noreply.github.com>
2023-11-14 05:15:35 +0000

    Fix styling

--- src/IntelimentServiceProvider.php
+++ src/IntelimentServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace ChrisReedIO\Inteliment;
 
+use ChrisReedIO\Inteliment\Commands\IntelimentCommand;
+use ChrisReedIO\Inteliment\Testing\TestsInteliment;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use ChrisReedIO\Inteliment\Commands\IntelimentCommand;
-use ChrisReedIO\Inteliment\Testing\TestsInteliment;
 
 class IntelimentServiceProvider extends PackageServiceProvider
 {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace ChrisReedIO\Inteliment\Tests;
 
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
+use ChrisReedIO\Inteliment\IntelimentServiceProvider;
 use Filament\Actions\ActionsServiceProvider;
 use Filament\FilamentServiceProvider;
 use Filament\Forms\FormsServiceProvider;
@@ -18,7 +19,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use ChrisReedIO\Inteliment\IntelimentServiceProvider;
 
 class TestCase extends Orchestra
 {
