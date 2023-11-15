cc31c0c5fb99ce311da6da8215eb596819e0f4c4
Chris Reed <chris@reedtech.us>
2023-11-14 23:34:40 -0600

    Remove unused Role import from AssistantResource

--- src/Resources/AssistantResource.php
+++ src/Resources/AssistantResource.php
@@ -9,7 +9,6 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Spatie\Permission\Models\Role;
 use function __;
 use function config;
 
